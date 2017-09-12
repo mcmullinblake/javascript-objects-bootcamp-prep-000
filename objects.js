da4fab28791a6b538ec9b3440e1fffb0fa488d56
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-12 00:14:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = new Object({ artistName: 'songTitle' })
 
 function updatePlaylist(obj, key, value) {
@@ -7,4 +8,8 @@ function updatePlaylist(obj, key, value) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist["Kanye"];
   return playlist
+=======
+var playlist = {
+  artist name: "song title"
+>>>>>>> 4b06c869d51f010e4b8d5c66e2b30688e251face
 }
